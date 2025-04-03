3d73a46f2c255fac072658fd0a7e0ff4ab5377ad
Shupto Rahman <shuptorahman8@gmail.com>
2025-04-04 01:29:35 +0600

    day12 completed

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/itemwidget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,13 +10,21 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(
+      20,
+      (index) => CatalogModel.items[0],
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(title: Text("Catalog App")),
       drawer: MyDrawer(),
-      body: Center(
-        child: Container(
-          child: Text("$days Days $name Tutorials."),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index]);
+          },
         ),
       ),
     );
