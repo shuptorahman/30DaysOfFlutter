7c052fc95829fd29b9e1b9f22afc73c23d086784
Shupto Rahman <shuptorahman8@gmail.com>
2025-04-18 23:16:14 +0600

    day16 completed_jsonMapPlusGridDone

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-import 'package:flutter_catalog/widgets/itemwidget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -47,13 +46,55 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(16.0),
         child:
             (CatalogModel.items.isNotEmpty)
-                ? ListView.builder(
-                  itemCount: CatalogModel.items.length,
+                ? GridView.builder(
+                  gridDelegate:
+                      SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 20,
+                        crossAxisSpacing: 20,
+                      ),
                   itemBuilder: (context, index) {
-                    return ItemWidget(
-                      item: CatalogModel.items[index],
+                    final item = CatalogModel.items[index];
+                    return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                      ),
+                      child: GridTile(
+                        header: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                          ),
+                          child: Text(
+                            item.name,
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        footer: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple[200],
+                          ),
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        child: Image.network(
+                          item.image,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
                     );
                   },
+                  itemCount: CatalogModel.items.length,
                 )
                 : Center(
                   child: CircularProgressIndicator(),
